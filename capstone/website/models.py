aad7bcbb6db9fd0c0ecb81b5124e5d8a07da7cf8
Bradley Tito <brad.tito@hotmail.com>
2023-08-21 20:42:52 +0200

    docstrings

--- capstone/website/models.py
+++ capstone/website/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Endorser(models.Model):
+    """Module that captures the name and surname of all endorsers and stores it in the database.
+    """
+
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
 
